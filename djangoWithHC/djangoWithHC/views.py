624dd7d77f6a9a94b5a9381f2fb37c564593c38d
Vaibhav Prajapat <vaibhav.prajapat.7725@gmail.com>
2024-12-10 15:03:45 +0530

    learnt to render static files and templates

--- djangoWithHC/djangoWithHC/views.py
+++ djangoWithHC/djangoWithHC/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello. This is the home page.")
+    # return HttpResponse("Hello. This is the home page.")
+    return render(request, "website/index.html")
 
 def about(request):
     return HttpResponse("Hello. This is the about page.")
