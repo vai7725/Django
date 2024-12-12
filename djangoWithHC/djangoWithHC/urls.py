3deea6ba47d2824742c6a3a6cfc44e36c523a75c
Vaibhav Prajapat <vaibhav.prajapat.7725@gmail.com>
2024-12-12 22:42:59 +0530

    Dynamic templates added

--- djangoWithHC/djangoWithHC/urls.py
+++ djangoWithHC/djangoWithHC/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -24,4 +24,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
+    path("newapp/", include('newApp.urls')),
 ]
--- djangoWithHC/newApp/views.py
+++ djangoWithHC/newApp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def all(request):
+    return render(request, 'newApp/all.html')
